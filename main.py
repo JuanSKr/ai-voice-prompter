736eff6396dcb6dc9a7acc09b787d467733683bc
Juancg <mr.juancg@gmail.com>
2023-10-07 21:49:33 +0200

    Listen command
    Now the voice will only be transcribed when the user says 'prueba' before

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import speech_recognition
 import pyttsx3
 
-recognizer = speech_recognition.Recognizer()
 
-while True:
-    try:
-        with speech_recognition.Microphone() as mic:
+def startToListen():
+    recognizer = speech_recognition.Recognizer()
 
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
+    while True:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+                text = recognizer.recognize_google(audio, language="es")
+                text = text.lower()
 
-            text = recognizer.recognize_google(audio, language='es')
-            text = text.lower()
+                if text == "prueba":
+                    print("Listening...")
+                    transcript()
+        except speech_recognition.UnknownValueError:
+            continue
 
-            print(f"Recognized {text}")
 
-    except speech_recognition.UnknownValueError():
+def transcript():
+    recognizer = speech_recognition.Recognizer()
 
-        recognizer = speech_recognition.Recognizer
-        continue
+    while True:
+        try:
+            with speech_recognition.Microphone() as mic:
+                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                audio = recognizer.listen(mic)
+                text = recognizer.recognize_google(audio, language="es")
+                text = text.lower()
+
+                print(f"Voice: {text}")
+        except speech_recognition.UnknownValueError:
+            continue
+
+
+startToListen()
