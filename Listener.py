fc186b7bb73c9f2c1ad3f48f0906d19d631a893d
Juancg <mr.juancg@gmail.com>
2023-10-08 12:29:33 +0200

    Main class created
    · Now the voicePrompter method will be called from a Main class.
    · In addition, the Config.py file has been created with information to be modified by the user.

--- Listener.py
+++ Listener.py
@@ -1,40 +1,35 @@
 import speech_recognition as sr
-import time
 import Prompter
 
-keep_listen = True
 
+class Listener:
+    def __init__(self):
+        pass
 
-def voicePrompter():
-    arx: bool
+    def voicePrompter(self):
+        recognizer = sr.Recognizer()
 
-    recognizer = sr.Recognizer()
+        while True:
+            try:
+                with sr.Microphone() as mic:
+                    recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                    audio = recognizer.listen(mic)
+                    text = recognizer.recognize_google(audio, language="es")
+                    text = text.lower()
 
-    while True:
-        try:
-            with sr.Microphone() as mic:
-                recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                audio = recognizer.listen(mic)
-                text = recognizer.recognize_google(audio, language="es")
-                text = text.lower()
+                    if text == "prueba":
+                        print("Listening...")
+                        arx = True
+                        while arx:
+                            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+                            prompt_audio = recognizer.listen(mic)
+                            prompt_text = recognizer.recognize_google(
+                                prompt_audio, language="es"
+                            )
+                            prompt_text = prompt_text.lower()
+                            arx = False
+                            print(prompt_text)
+                            Prompter.prompter(prompt_text)
 
-                if text == "prueba":
-                    print("Starting arx")
-                    print("Listening...")
-                    arx = True
-                    while arx:
-                        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-                        prompt_audio = recognizer.listen(mic)
-                        prompt_text = recognizer.recognize_google(
-                            prompt_audio, language="es"
-                        )
-                        prompt_text = prompt_text.lower()
-                        arx = False
-                        print(prompt_text)
-                        Prompter.prompter(prompt_text)
-
-        except sr.UnknownValueError:
-            continue
-
-
-voicePrompter()
+            except sr.UnknownValueError:
+                continue
---
+++ Main.py
@@ -0,0 +1,15 @@
+import Listener
+
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        listener = Listener.Listener()
+        listener.voicePrompter()
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
